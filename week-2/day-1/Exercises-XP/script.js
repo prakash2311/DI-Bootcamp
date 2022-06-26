5c593b854f168112273815ef34ef1e92c60b18f9
Prakash Devta <prakashdevta@Prakashs-MacBook-Air.local>
2022-06-26 15:18:00 +0300

    week2 day1 work

---
+++ week-2/day-1/Exercises-XP/script.js
@@ -0,0 +1,41 @@
+
+//Exe-1
+
+//let food = 'chocolate';
+//let meal = 'dinner';
+//let sentenceTemplate = `I eat ${food} at every ${meal}`;
+//console.log(sentenceTemplate);
+
+
+//Ex--2-1
+let myWatchedSeries =["black mirror", "money heist", "the big bang theory"];
+//let myWatchedSeriesLength = myWatchedSeries.length;
+//console.log(myWatchedSeriesLength);
+
+
+//Ex--1-2
+//console.log(`i watched 3 series ${myWatchedSeries[0]}, ${myWatchedSeries[1]} ${myWatchedSeries[2]}`);
+
+//part-2-1
+//myWatchedSeries.splice(2,1,"friends");
+//console.log(myWatchedSeries);
+
+ //part2-2
+
+//myWatchedSeries.push("game of throne");
+//console.log(myWatchedSeries);
+
+//Ex--3
+let temperatureC = 5 ;
+let temperatureF =((5/5)*9+32);
+console.log(temperatureF);
+
+//EX -- 4
+
+let c;
+let a=34;
+let b =21;
+//console.log(a+b);
+a = 2;
+
+console.log(a+b);
---
+++ week-2/day-1/pre-exc/script.js
@@ -0,0 +1,21 @@
+
+//ex--1
+
+//let addressNumber = 32;
+//let adressStreet ="ashqlon";
+//let country ="israel";
+//let globalAddress="No:" + addressNumber +"street" + adressStreet + "country" + country;
+//console.log(globalAddress);
+
+//ex--2
+//const birthYear= 1987;
+//let futureYear=2050;
+//let futureAge= futureYear-birthYear;
+//console.log(``)
+
+//ex-3
+let pets =['cat','dog','rabbit','cow'];
+//console.log(pets[1]);
+//pets.splice(2,1,"horse")
+//console.log(pets)
+console.log(`pets.length`);
