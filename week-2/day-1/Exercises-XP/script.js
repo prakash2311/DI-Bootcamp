af13df47ec832ad2b8d7873238567f932424d26c
Prakash Devta <prakashdevta@Prakashs-MacBook-Air.local>
2022-06-27 08:19:42 +0300

    finished working on the main branch

--- week-2/day-1/Exercises-XP/script.js
+++ week-2/day-1/Exercises-XP/script.js
@@ -1,10 +1,10 @@
 
 //Exe-1
 
-//let food = 'chocolate';
-//let meal = 'dinner';
-//let sentenceTemplate = `I eat ${food} at every ${meal}`;
-//console.log(sentenceTemplate);
+let food = 'chocolate';
+let meal = 'dinner';
+let sentenceTemplate = `I eat ${food} at every ${meal}`;
+console.log(sentenceTemplate);
 
 
 //Ex--2-1
@@ -14,20 +14,20 @@ let myWatchedSeries =["black mirror", "money heist", "the big bang theory"];
 
 
 //Ex--1-2
-//console.log(`i watched 3 series ${myWatchedSeries[0]}, ${myWatchedSeries[1]} ${myWatchedSeries[2]}`);
+console.log(`i watched 3 series ${myWatchedSeries[0]}, ${myWatchedSeries[1]} ${myWatchedSeries[2]}`);
 
 //part-2-1
-//myWatchedSeries.splice(2,1,"friends");
-//console.log(myWatchedSeries);
+myWatchedSeries.splice(2,1,"friends");
+console.log(myWatchedSeries);
 
  //part2-2
 
-//myWatchedSeries.push("game of throne");
-//console.log(myWatchedSeries);
+myWatchedSeries.push("game of throne");
+console.log(myWatchedSeries);
 
 //Ex--3
 let temperatureC = 5 ;
-let temperatureF =((5/5)*9+32);
+let temperatureF =((temperatureC/5)*9+32);
 console.log(temperatureF);
 
 //EX -- 4
