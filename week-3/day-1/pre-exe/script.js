24e7dc961cf7f63ffb7fc44fcd62a5eab7a0ba3c
Prakash Devta <prakashdevta@Prakashs-MacBook-Air.local>
2022-07-03 22:49:23 +0300

    week3 day1

--- week-3/day-1/pre-exe/script.js
+++
@@ -1,6 +0,0 @@
-let divs = document.getElementById('content')
-console.log(divs.lastElementChild);
-console.log(divs.nextElementSibling);
-console.log(divs.previousElementSibling);
-
-console.log('hello');
